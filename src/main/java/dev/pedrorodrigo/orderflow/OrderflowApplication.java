0f1bed9696027a832066dc8cbc25828e068ff4d1
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-09-22 16:34:23 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/dev/pedrorodrigo/orderflow/OrderflowApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrorodrigo.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowApplication.class, args);
+	}
+
+}
