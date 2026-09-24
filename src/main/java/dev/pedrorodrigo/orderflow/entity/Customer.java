49d05d1958caf297eb1deb6d8dc36839d93a959c
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-09-24 14:54:55 -0300

    feat: add Customer entity

---
+++ src/main/java/dev/pedrorodrigo/orderflow/entity/Customer.java
@@ -0,0 +1,41 @@
+package dev.pedrorodrigo.orderflow.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Pattern(regexp = "\\d{11}", message = "CPF must contain exactly 11 digits")
+    @Column(unique = true, nullable = false, length = 11)
+    private String cpf;
+
+    @Email
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+}
